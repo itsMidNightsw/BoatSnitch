45daac285fff0e9ab41fbf4e5cc2f2840f6bd0d0
MidNight <adam.bernard22@gmail.com>
2026-01-03 13:45:58 +0100

    works for multiple boxes now

--- show_training_data.py
+++ show_training_data.py
@@ -18,29 +18,41 @@ def id_to_path(fileid, image_or_label):
 
 def read_label(label_path):
     """ Reads the .txt file and returns the denormalized labels"""
+    all_labels = []
     with open(label_path, 'r') as f:
-        lines = f.readline().split()
-        _, x_c, y_c, w, l = [float(x) for x in lines]
-        return x_c*256, y_c*256, w*256, l*256
+        for line in f:
+            elements = line.split()
+            if not elements:
+                continue
+            _, x_c, y_c, w, l = [float(x) for x in elements]
+            all_labels.append((x_c * 256, y_c * 256, w * 256, l * 256))
+    return all_labels
+
+print(read_label(label_path = id_to_path("Sen_ship_hv_02017071802012015", "label"))) # test
 
 
 
-#print(read_label(label_path = id_to_path("Sen_ship_hv_0201705190104025", "label"))) # test
-
-
-def show_image_box(img_id: str):
+def show_image_boxes(img_id: str):
     """ Reads the id and returns the image with the boxes around it"""
     image = Image.open(id_to_path(img_id, 'image'))
     fig, ax = plt.subplots()
     ax.imshow(image, cmap='gray')
 
-    x_c, y_c, w, l = read_label(label_path = id_to_path(img_id, 'label'))
-    box = patches.Rectangle((x_c - w/2, y_c - l/2), w, l, linewidth=2, edgecolor='b', facecolor='none')
-    ax.add_patch(box)
+    labels = read_label(label_path = id_to_path(img_id, 'label'))
+
+    for x_c, y_c, w, l in labels:
+        box = patches.Rectangle(
+            (x_c - w / 2, y_c - l / 2),
+            w, l,
+            linewidth=2,
+            edgecolor='b',
+            facecolor='none'
+        )
+        ax.add_patch(box)
 
     plt.show()
 
-show_image_box(img_id = "Sen_ship_hv_02017071802012015")
+show_image_boxes(img_id = "Sen_ship_hv_02017071802012015")
 
 
 
